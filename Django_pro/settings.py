0364e984bf1410f9bb38e4a3d70a60f014d00ed9
Sumit Sharma <sumit305sharma@gmail.com>
2019-02-03 12:58:45 +0530

    Update settings.py

--- Django_pro/settings.py
+++ Django_pro/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'urblogpost@gmail.com'
-EMAIL_HOST_PASSWORD = 'need@code'
+EMAIL_HOST_USER = '****@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
